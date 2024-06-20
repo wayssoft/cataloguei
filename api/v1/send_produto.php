130f7332e26f6ab44e2b7a068672d791d1107a2a
Rick <ricardolemoscapelari@outlook.com>
2024-06-19 21:37:44 -0300

    update

--- api/v1/send_produto.php
+++ api/v1/send_produto.php
@@ -78,19 +78,101 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
             $_ACTION = 'edit';
         }
 
+
+        #|
+        #|  Parte do codigo dedicada a tratamento da imagem
+        #|_______________________________________________________________________________________
+        #|
+        #|
+        #| 
+        
+        try {
+
+            // String base64 a ser convertida
+            $base64_string = $imgB64;
+            // Decodifica a string base64
+            $image_data = base64_decode($base64_string);
+            // Define o caminho e nome do arquivo onde a imagem será salva
+            $nome_final = md5($Identificador.'-'.time().'-'.$id_empresa).'.jpg';
+            $file_path = '../../app/painel/uploads/'.$nome_final;
+            // Salva a imagem no arquivo
+            file_put_contents($file_path, $image_data);
+
+            // Pasta onde o arquivo vai ser salvo
+            $_UP['pasta'] = '../../app/painel/uploads/';
+
+            // Caminho para a imagem original
+
+            $imagem_original = $_UP['pasta'] . $nome_final;
+
+
+            // Novas dimensões desejadas
+            $nova_largura = 320; // Largura desejada em pixels
+            $nova_altura = 250; // Altura desejada em pixels
+
+            // Criando uma imagem em branco com as novas dimensões
+            $nova_imagem = imagecreatetruecolor($nova_largura, $nova_altura);
+            $cor_transparente = imagecolorallocatealpha($nova_imagem, 0, 0, 0, 127); // Cor transparente
+            imagefill($nova_imagem, 0, 0, $cor_transparente);
+            imagesavealpha($nova_imagem, true); // Salvar transparência
+
+            // Carregando a imagem original
+            $imagem_original = imagecreatefromjpeg($imagem_original);
+
+            // Redimensionando a imagem original para a nova imagem
+            imagecopyresampled($nova_imagem, $imagem_original, 0, 0, 0, 0, $nova_largura, $nova_altura, imagesx($imagem_original), imagesy($imagem_original));
+
+            // Calculando as coordenadas para o corte no centro da imagem
+            $corte_x = ($nova_largura - 320) / 2;
+            $corte_y = ($nova_altura - 250) / 2;
+
+            // Criando uma nova imagem para o corte com fundo transparente
+            $nova_imagem_cortada = imagecreatetruecolor(320, 250);
+            imagefill($nova_imagem_cortada, 0, 0, $cor_transparente);
+
+            // Cortando a imagem no centro
+            imagecopy($nova_imagem_cortada, $nova_imagem, 0, 0, $corte_x, $corte_y, 320, 250);
+
+            // Salvar a nova imagem cortada como PNG (substituir a imagem original se desejar)
+            $nome_img_crop = time();
+            $nome_img_crop = md5($nome_img_crop);
+            $nome_img_crop = $nome_img_crop.'.png';
+            imagepng($nova_imagem_cortada, $_UP['pasta'] . $nome_img_crop);
+
+            // Liberar memória
+            imagedestroy($nova_imagem);
+            imagedestroy($nova_imagem_cortada);
+            imagedestroy($imagem_original);
+
+            // delata o arquivo antigo
+            if(file_exists( $_UP['pasta'] . $nome_final )){
+                unlink($_UP['pasta'] . $nome_final);
+            }
+
+            $path_imagem = 'uploads/' . $nome_img_crop;
+
+            
+        } catch (Exception $e) {
+            $response['status']  = 'error'; $response['message'] = $e->getMessage();
+            header("HTTP/1.1 400 Invalid request");
+            exit;
+        }
+
+
+
         //faz inserção dos dados
         if($_ACTION == 'insert')
         {
 
             // Prepara a consulta SQL para inserção dos dados
-            $sql = "INSERT INTO produto (codigo_barras, nome, descricao, preco, estoque, identificador, id_empresa)  VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $sql = "INSERT INTO produto (codigo_barras, nome, descricao, preco, estoque, identificador, path_imagem, id_empresa)  VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             $stmt = $mysqli->prepare($sql);
             if (!$stmt) {
             //echo "Erro na preparação da consulta: " . $conn->error;
             $response['status']  = 'error'; $response['message'] = 'Interno db cod[001]';
             return -1;
             }
-            $stmt->bind_param("sssssss", $codigo_barras, $nome, $descricao, $preco, $estoque, $Identificador, $id_empresa);
+            $stmt->bind_param("ssssssss", $codigo_barras, $nome, $descricao, $preco, $estoque, $Identificador,$path_imagem, $id_empresa);
             // Executa a consulta
             if ($stmt->execute()) {
                 $id = $mysqli->insert_id; // Obtém o ID do registro inserido
@@ -111,7 +193,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         if($_ACTION == 'edit')
         {
 
-            $sql = "UPDATE produto SET codigo_barras=?, nome=?, descricao=?, preco=?, estoque=? WHERE id = ?"; // Você pode ajustar a condição WHERE conforme necessário
+            $sql = "UPDATE produto SET codigo_barras=?, nome=?, descricao=?, preco=?, estoque=?, path_imagem=? WHERE id = ?"; // Você pode ajustar a condição WHERE conforme necessário
             $stmt = $mysqli->prepare($sql);
             if (!$stmt) {
                 //echo "Erro na preparação da consulta: " . $conn->error;
@@ -119,7 +201,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
                 return -1;
             }
             $status = 'awaiting_approval';
-            $stmt->bind_param("ssssss", $codigo_barras, $nome, $descricao,$preco,$estoque,$id_produto);
+            $stmt->bind_param("sssssss", $codigo_barras, $nome, $descricao,$preco,$estoque,$path_imagem,$id_produto);
             // Executa a consulta de atualização
             if ($stmt->execute()) {
                 $response['status']     = 'success';
